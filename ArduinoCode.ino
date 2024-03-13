c0010cf1fb7b7ee0d4bda333f11d0ce95514d5a4
Zakir Shikhli <105371830+shikhli@users.noreply.github.com>
2024-03-13 09:33:21 +0400

    Update ArduinoCode.ino

--- ArduinoCode.ino
+++ ArduinoCode.ino
@@ -1,3 +1,12 @@
+#include <Dictionary.h>
+Dictionary<char, unsigned char*> MyDict;
+
+
+
+
+////////////////////////////////
+// MOTOR VARIABLES
+
 int motorsRight_analog = 9;
 int motorsRight_digital = 2;
 
@@ -11,6 +20,10 @@ float directionValue = 1.0f;
 int dirLR = 0;
 int dirFB = 0;
 
+// MOTOR VARIABLES
+////////////////////////////////
+
+
 ////////////////////////////////
 // LED VARIABLES
 
@@ -18,80 +31,104 @@ int dirFB = 0;
 #define IIC_SCL A5
 #define IIC_SDA A4
 
-unsigned char data_line = 0;
-unsigned char delay_count = 0;
-unsigned char data_display1 = 0;
-unsigned char data_display2 = 0;
-unsigned char data_display3 = 0;
-unsigned char data_display4 = 0;
-unsigned char data_display5 = 0;
-unsigned char data_display6 = 0;
-unsigned char data_display7 = 0;
-unsigned char data_display8 = 0;
-unsigned char data_display9 = 0;
-unsigned char data_display10 = 0;
-unsigned char data_display11 = 0;
-unsigned char data_display12 = 0;
-unsigned char data_display13 = 0;
-unsigned char data_display14 = 0;
-unsigned char data_display15 = 0;
-unsigned char data_display16 = 0;
 void IIC_start();
 void IIC_send(unsigned char send_data);
 void IIC_end();
-unsigned char messageTable[5][16] = {
-  { 0x00, 0x00, 0x00, 0x00, 0x00, 0x42, 0x24, 0xff, 0xdb, 0x7e, 0x24, 0x00, 0x00, 0x00, 0x00, 0x00 },  //     BT
-  { 0x00, 0x00, 0x18, 0x24, 0x00, 0x42, 0x24, 0xff, 0xdb, 0x7e, 0x24, 0x00, 0x24, 0x18, 0x00, 0x00 },  //    (BT)
-  { 0x3c, 0x42, 0x18, 0x24, 0x00, 0x42, 0x24, 0xff, 0xdb, 0x7e, 0x24, 0x00, 0x24, 0x18, 0x42, 0x3c },  //  (( BT ))
-  { 0x3c, 0x42, 0x00, 0x00, 0x00, 0x42, 0x24, 0xff, 0xdb, 0x7e, 0x24, 0x00, 0x00, 0x00, 0x42, 0x3c },  //   ( BT )
-  { 0x00, 0x00, 0x00, 0x00, 0x00, 0x42, 0x24, 0xff, 0xdb, 0x7e, 0x24, 0x00, 0x00, 0x00, 0x00, 0x00 }   //     BT
+
+
+
+// ALPHABET
+unsigned char char_A[3] = { 0xf0, 0x48, 0xf8 };
+unsigned char char_B[3] = { 0xf8, 0xa8, 0x50 };
+unsigned char char_C[3] = { 0x70, 0x88, 0x88 };
+unsigned char char_D[3] = { 0xf8, 0x88, 0x70 };
+unsigned char char_E[3] = { 0xf8, 0xa8, 0x88 };
+unsigned char char_F[3] = { 0xf8, 0x28, 0x08 };
+unsigned char char_G[3] = { 0xf8, 0x88, 0xe8 };
+unsigned char char_H[3] = { 0xf8, 0x20, 0xf8 };
+unsigned char char_I[3] = { 0x88, 0xf8, 0x88 };
+unsigned char char_J[3] = { 0x48, 0x88, 0x78 };
+unsigned char char_K[3] = { 0xf8, 0x20, 0xd8 };
+unsigned char char_L[3] = { 0xf8, 0x80, 0xc0 };
+unsigned char char_M[3] = { 0xf8, 0x10, 0xf8 };
+unsigned char char_N[3] = { 0xf8, 0x08, 0xf0 };
+unsigned char char_O[3] = { 0x70, 0x88, 0x70 };
+unsigned char char_P[3] = { 0xf8, 0x28, 0x38 };
+unsigned char char_Q[3] = { 0x78, 0xc8, 0x78 };
+unsigned char char_R[3] = { 0xf8, 0x28, 0xd0 };
+unsigned char char_S[3] = { 0xb0, 0xa8, 0x68 };
+unsigned char char_T[3] = { 0x08, 0xf8, 0x08 };
+unsigned char char_U[3] = { 0xf8, 0x80, 0xf8 };
+unsigned char char_V[3] = { 0x78, 0x80, 0x78 };
+unsigned char char_W[3] = { 0xf8, 0x40, 0xf8 };
+unsigned char char_X[3] = { 0xd8, 0x20, 0xd8 };
+unsigned char char_Y[3] = { 0x18, 0xe0, 0x18 };
+unsigned char char_Z[3] = { 0xc8, 0xa8, 0x98 };
+
+// NUMBERS
+unsigned char char_0[3] = { 0xf8, 0x88, 0xf8 };
+unsigned char char_1[3] = { 0x20, 0x10, 0xf8 };
+unsigned char char_2[3] = { 0xe8, 0xa8, 0xb8 };
+unsigned char char_3[3] = { 0x88, 0xa8, 0xf8 };
+unsigned char char_4[3] = { 0x60, 0x50, 0xf8 };
+unsigned char char_5[3] = { 0xb8, 0xa8, 0xe8 };
+unsigned char char_6[3] = { 0xf0, 0xa8, 0xe8 };
+unsigned char char_7[3] = { 0x18, 0x08, 0xf8 };
+unsigned char char_8[3] = { 0xf8, 0xa8, 0xf8 };
+unsigned char char_9[3] = { 0xb8, 0xa8, 0x78 };
+
+// Symbols
+unsigned char char_question[3] = { 0x08, 0xa8, 0x38 };
+unsigned char char_dot[3] = { 0x80, 0x00, 0x00 };
+unsigned char char_tire[3] = { 0x20, 0x20, 0x20 };
+
+String bt_id;
+
+unsigned char tableEmpty[16] = {
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
 };
 
-unsigned char patternTable[3][16] = {
-  { 0xb7, 0xdd, 0x77, 0xfe, 0xdb, 0xee, 0x7f, 0xdb, 0xf7, 0xbd, 0xff, 0xd7, 0x7d, 0xdf, 0xf6, 0x7d },  // Pattern P1
-  { 0x77, 0xbd, 0xef, 0xfe, 0xb7, 0xfd, 0x6f, 0xba, 0xdf, 0x7b, 0xfd, 0xaf, 0xf5, 0x7b, 0xff, 0xdd },  // Pattern P2
-  { 0xbd, 0xea, 0xff, 0x5b, 0xfd, 0xee, 0xaf, 0xda, 0xf7, 0xbe, 0xe7, 0x7d, 0xef, 0xbf, 0xdb, 0x7e }   // Pattern P3
-};
-
-unsigned char fullTable[1][16] = {
-  { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff }  // Full
-};
-
-unsigned char carNumberTable[1][16] = {
-  { 0x52, 0xaa, 0x52, 0x02, 0xfa, 0xaa, 0x52, 0x02, 0x22, 0x02, 0xf2, 0x2a, 0xf2, 0x02, 0xaa, 0xfa }  // 8B-A3 (MAC number ending)
-};
-
-unsigned char emptyTable[1][16] = {
-  { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }  // Empty
-};
-
-int linesCount;
+unsigned char* table = tableEmpty;
+bool ledEnabled = true;
 
 // LED VARIABLES
 ////////////////////////////////
 
 
 void setup() {
-  Serial.begin(9600);
+  // BT
+  BTaddressReader();
 
+  // MOTORS
   pinMode(motorsRight_analog, OUTPUT);
   pinMode(motorsLeft_analog, OUTPUT);
   pinMode(motorsRight_digital, OUTPUT);
   pinMode(motorsLeft_digital, OUTPUT);
-
-  // LED SETUP
-  pinMode(IIC_SCL, OUTPUT);
-  pinMode(IIC_SDA, OUTPUT);
-  digitalWrite(IIC_SCL, LOW);
-  digitalWrite(IIC_SDA, LOW);
 }
 
-bool ledEnabled = false;
 
 void loop() {
+  MotorStuff();
+}
 
-  LEDstuff();
 
+
+void MotorStuff() {
   while (Serial.available() > 0) {
     BTCommand = Serial.read();
     Serial.println(BTCommand);
@@ -100,11 +137,13 @@ void loop() {
     switch (BTCommand) {
 
       // LED
-      case 'O':
+      case '.':
         ledEnabled = false;
+        ApplyLED();
         break;
-      case 'I':
+      case ',':
         ledEnabled = true;
+        ApplyLED();
         break;
 
 
@@ -146,11 +185,7 @@ void loop() {
         motorPower = 255;
         break;
 
-
-
-
-
-
+        //////////////////////////
 
       case ')':  //0
         dirLR = -4;
@@ -267,50 +302,35 @@ void loop() {
 }
 
 
-
 void LEDstuff() {
   /**************set the address plus 1***************/
   IIC_start();
   IIC_send(0x40);  // set the address plus 1 automatically
   IIC_end();
   /************end the process of address plus 1 *****************/
+
+
   /************set the data display*****************/
   IIC_start();
   IIC_send(0xc0);  // set the initial address as 0
 
-
-  if (ledEnabled) {
-    for (char i = 0; i < 16; i++) {
-      IIC_send(messageTable[data_line][i]);  // send the display data
-    }
-    linesCount = 5;
-  } else {
-    for (char i = 0; i < 16; i++) {
-      IIC_send(emptyTable[data_line][i]);  // send the display data
-    }
-    linesCount = 1;
+  for (char i = 0; i < 16; i++) {
+    IIC_send(table[i]);  // send the display data
   }
-
-
-  if (++delay_count >= 10) {
-    delay_count = 0;
-    data_line++;
-    if (data_line >= linesCount) {
-      data_line = 0;
-    }
-  }
-
   IIC_end();
   /************end the data display*****************/
+
+
   /*************set the brightness display***************/
   IIC_start();
   IIC_send(0x8A);  // set the brightness display
   IIC_end();
   /*************end the brightness display***************/
-  delay(100);
 }
 
-// LED STUFF
+
+
+// LED FUNCTIONS
 void IIC_start() {
   digitalWrite(IIC_SCL, LOW);
   delayMicroseconds(3);
@@ -345,4 +365,107 @@ void IIC_end() {
   delayMicroseconds(3);
   digitalWrite(IIC_SDA, HIGH);
   delayMicroseconds(3);
+}
+
+
+// BT MAC TO LED
+void BTaddressReader() {
+  Serial.begin(9600);  // 9600
+  delay(1000);
+  Serial.println("AT+ADDR?");  // "AT+ADDR?"
+
+  bt_id = "BT-?";
+  unsigned long startedWaiting = millis();
+  unsigned long howLongToWait = 2000L;
+  while (Serial.available() <= 0 && millis() - startedWaiting <= howLongToWait) {}
+
+  if (Serial.available() > 0) {
+    String s = Serial.readString();
+    Serial.println(s);
+    bt_id = s.substring(s.length() - 6, s.length());
+  }
+
+  Serial.println(bt_id);
+
+
+  // CREATE SYMBOL DICTIONARY
+  MyDict.set('0', char_0);
+  MyDict.set('1', char_1);
+  MyDict.set('2', char_2);
+  MyDict.set('3', char_3);
+  MyDict.set('4', char_4);
+  MyDict.set('5', char_5);
+  MyDict.set('6', char_6);
+  MyDict.set('7', char_7);
+  MyDict.set('8', char_8);
+  MyDict.set('9', char_9);
+
+  MyDict.set('A', char_A);
+  MyDict.set('B', char_B);
+  MyDict.set('C', char_C);
+  MyDict.set('D', char_D);
+  MyDict.set('E', char_E);
+  MyDict.set('F', char_F);
+  MyDict.set('G', char_G);
+  MyDict.set('H', char_H);
+  MyDict.set('I', char_I);
+  MyDict.set('J', char_J);
+  MyDict.set('K', char_K);
+  MyDict.set('L', char_L);
+  MyDict.set('M', char_M);
+  MyDict.set('N', char_N);
+  MyDict.set('O', char_O);
+  MyDict.set('P', char_P);
+  MyDict.set('Q', char_Q);
+  MyDict.set('R', char_R);
+  MyDict.set('S', char_S);
+  MyDict.set('T', char_T);
+  MyDict.set('U', char_U);
+  MyDict.set('V', char_V);
+  MyDict.set('W', char_W);
+  MyDict.set('X', char_X);
+  MyDict.set('Y', char_Y);
+  MyDict.set('Z', char_Z);
+
+  MyDict.set('?', char_question);
+  MyDict.set('.', char_dot);
+  MyDict.set('-', char_tire);
+
+
+  ApplyLED();
+}
+
+unsigned char newTable[16];
+
+void ApplyLED() {
+
+  if (ledEnabled) {
+    newTable[0] = MyDict.get(bt_id.charAt(0))[0];
+    newTable[1] = MyDict.get(bt_id.charAt(0))[1];
+    newTable[2] = MyDict.get(bt_id.charAt(0))[2];
+    newTable[3] = 0x00;
+    newTable[4] = MyDict.get(bt_id.charAt(1))[0];
+    newTable[5] = MyDict.get(bt_id.charAt(1))[1];
+    newTable[6] = MyDict.get(bt_id.charAt(1))[2];
+    newTable[7] = 0x00;
+    newTable[8] = 0x00;
+    newTable[9] = MyDict.get(bt_id.charAt(2))[0];
+    newTable[10] = MyDict.get(bt_id.charAt(2))[1];
+    newTable[11] = MyDict.get(bt_id.charAt(2))[2];
+    newTable[12] = 0x00;
+    newTable[13] = MyDict.get(bt_id.charAt(3))[0];
+    newTable[14] = MyDict.get(bt_id.charAt(3))[1];
+    newTable[15] = MyDict.get(bt_id.charAt(3))[2];
+
+    table = newTable;
+  } else {
+    table = tableEmpty;
+  }
+
+  pinMode(IIC_SCL, OUTPUT);
+  pinMode(IIC_SDA, OUTPUT);
+  digitalWrite(IIC_SCL, LOW);
+  digitalWrite(IIC_SDA, LOW);
+
+  LEDstuff();
 }
